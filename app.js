553fdf66deb30053103da878299a357447527b58
amnad2003 <birdxoc223@gmail.com>
2025-12-31 01:09:25 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+fetch("rates.json")
+.then(res => res.json())
+.then(data => render(data))
+
+function render(data){
+  document.getElementById("shop").innerText = data.shopName
+
+  const box = document.getElementById("content")
+  box.innerHTML = ""
+
+  data.games.forEach(game => {
+    let html = `
+      <div class="card">
+        <h2>${game.name}</h2>
+    `
+
+    game.items.forEach(item => {
+      html += `
+        <div class="price-row">
+          <span>${item.name}</span>
+
+          <div class="price-right">
+            <b>${item.price} บาท</b>
+            <span class="cart-btn"
+              onclick="window.open('${data.orderLink}','_blank')"
+              title="สั่งซื้อ">
+              🛒
+            </span>
+          </div>
+        </div>
+      `
+    })
+
+    html += `</div>`
+    box.innerHTML += html
+  })
+}
